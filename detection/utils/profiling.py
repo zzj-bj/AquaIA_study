3081610c0ede6ed1463e1f3b58c82fb1133f3c2c
zzj-bj <zhijian2040.zhou@gmail.com>
2026-06-16 15:15:26 +0200

    profiling.py

--- detection/utils/profiling.py
+++ detection/utils/profiling.py
@@ -1,3 +1,5 @@
+# Z: PyTorch Profiler is a tool that allows the collection of execution performance 
+# during training and inference
 from torch.profiler import profile, ProfilerActivity, schedule
 
 
@@ -5,8 +7,11 @@ def torch_profiler_factory(wait=1, warmup=1, active=3):
     torch_profiler = profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
         schedule=schedule(
+            # Z: no recording for the first "wait" steps
             wait=wait,
+            # Z: warmup for "warmup" steps
             warmup=warmup,
+            # Z: record for "active" steps
             active=active,
         ),
     )
