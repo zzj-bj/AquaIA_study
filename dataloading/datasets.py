c2d08b1565f8f5241c302b234817f50c9c617a28
Zhijian ZHOU <zhijian2040.zhou@gmail.com>
2026-06-17 13:32:46 +0200

    datasets.py OK

--- dataloading/datasets.py
+++ dataloading/datasets.py
@@ -23,7 +23,9 @@ from detection.utils.config_utils import load_class_names
 @pipeline_def
 def create_detection_pipeline(dataset_src, stats, img_size=640, device="gpu"):
     """Z: DALI data preprocessing pipeline, used to read, decode, resize, and normalize JPG images
-    then output them for training."""
+    then output them for training. One image treatement pipeline.
+    DALI decode, resize, mean/std normalize, PyTorch CHW tensor (inputs)
+    """
     # Z: external_source() gets data from outside of DALI pipeline
     # Z: here dataset_src = self.dataset.__call__ = JpgDALIDataset.__call__
     # Z: and JpgDALIDataset.__call__() returns a tuple of (encoded_img, idx)
@@ -72,7 +74,7 @@ class BaseDetectionDataset:
     - normalization
 
     Z: This class handles dataset metadata and target loading:
-    - loads normalization statistics from stats.npy
+    - loads normalization statistics from stats.npy converted to [0 - 255]
     - loads class names and number of classes
     - builds a sorted list of label files
     - parses YOLO-format label files into class labels and bounding boxes
@@ -203,7 +205,10 @@ class BaseDetectionDataset:
 
 class JpgDALIDataset(BaseDetectionDataset):
     """Z: Mainly used for DALI's external_source, which means the following __call__
-    will be repeatedly called by DALI to read image bytes."""
+    will be repeatedly called by DALI to read image bytes. One image.
+    Train (__call__): JPG bytes, numpy uint8 buffer (encoded_img). Decode resize normalize will be done in DALI pipeline.
+    Non-train (__getitem__): JPG bytes, DALI decode, resize, PyTorch CHW tensor (sample["image"]).
+    Non-train (__getitem__): JPG bytes, DALI decode, resize, mean/std normalize, PyTorch CHW tensor (sample["input"])."""
     # TODO : only JPEG, need to think about TIFF handling
 
     def __init__(
@@ -277,10 +282,13 @@ class JpgDALIDataset(BaseDetectionDataset):
     def __getitem__(self, key):
         # Slow but useful for sampling a few images for visualization / testing
         # Mirrors the DALI pipeline path using DALI dynamic operators.
+        # Z: find the true dataset index
         idx = self.indices[key]
+        # Z: get image file name without extention
         img_id = self.target_files[idx].stem
         img_path = self.img_dir / f"{img_id}.{self.img_format}"
 
+        # Z: read original binary image bytes from disk and convert to numpy array of uint8
         # Z: .copy() is used to ensure that the numpy array has its own memory
         encoded_img = np.frombuffer(img_path.read_bytes(), dtype=np.uint8).copy()
         device = "gpu" if self.device == "cuda" else "cpu"
@@ -306,6 +314,7 @@ class JpgDALIDataset(BaseDetectionDataset):
         )
         # Z: convert DALI tensor to CHW PyTorch tensor
         img = self._dali_tensor_to_torch(img).cpu().permute(2, 0, 1)
+        # Z: norm_img is already in CHW format
         norm_img = self._dali_tensor_to_torch(norm_img)
         sample = {
             "image": img,
@@ -317,6 +326,9 @@ class JpgDALIDataset(BaseDetectionDataset):
 
 
 class JpgDetectionDataset(BaseDetectionDataset):
+    """Z: This class is used for non-DALI situations, where images are loaded and processed using PIL and NumPy.
+    One image.
+    JPG bytes, PIL decode, resize, CHW pytorch tensor (sample["image"]), mean/std normalize (sample["input"])."""
     def __init__(
         self,
         dataset_root: str,
@@ -349,11 +361,14 @@ class JpgDetectionDataset(BaseDetectionDataset):
 
 
 class DALIDetectionDataLoader:
+    """Z: wrap JpgDALIDataset into a DALI dataloader for training loop as `for batch in loader`"""
     def __init__(
         self,
         dataset,
         device="gpu",  # can be dropped and inferred from dataset, but keeping it explicit for now
+        # Z: nb threads used for DALI pipeline execution
         num_threads=3,
+        # Z: nb Python workers used by DALI when calling a Python external_source
         py_num_workers=3,
         py_start_method="spawn",
     ):
@@ -372,24 +387,33 @@ class DALIDetectionDataLoader:
         self.pipeline.build()
         self.loader = DALIRaggedIterator(
             pipelines=[self.pipeline],
+            # Z: define outputs, batch["inputs"] et batch["targets_idx"]
             output_map=["inputs", "targets_idx"],
+            # Z: declare two outputs are dense, each sample in a batch has same shape can be stacked into a tensor
             output_types=[
                 DALIRaggedIterator.DENSE_TAG,
                 DALIRaggedIterator.DENSE_TAG,
             ],
+            # Z: nb samples per epoch
             size=self.dataset.full_iterations * self.dataset.batch_size,
+            # Z: drop last batch if smaller than batch_size
             last_batch_policy=LastBatchPolicy.DROP,
+            # Z: reset iterator
             auto_reset=True,
         )
 
     def __len__(self):
+        # Z: return full batch nb
         return self.dataset.full_iterations
 
     def __iter__(self):
+        # Z: define iteration behavior
         return iter(self.loader)
 
 
 def parse_batch(batch):
+    """Z: Extract the model input "inputs" from "batch" outputted by dataloader
+    and also extract the img_paths if any."""
     if isinstance(batch, list):
         batch = batch[0]
     inputs = batch["inputs"]
@@ -399,24 +423,30 @@ def parse_batch(batch):
 
 
 def sample_indices(dataset_size, num_samples, seed):
+    """Z: randomly sample some sample indices from the dataset and return them sorted"""
     rng = random.Random(seed)
     sample_size = min(num_samples, dataset_size)
-    # Z: return sorted randomly sampled indices
     return sorted(rng.sample(range(dataset_size), sample_size))
 
 
 def detection_collate_fn(batch):
+    """Z: stack single sample into batch. Only for non-DALI situations."""
     collated_batch = {
+        # Z: [B, 3, H, W]
         "images": torch.stack([item["image"] for item in batch], dim=0),
         "inputs": torch.stack([item["input"] for item in batch], dim=0),
+        # Z: [indices]
         "targets_idx": [item["target_idx"] for item in batch],
+        # Z: [paths]
         "img_paths": [item["img_path"] for item in batch],
     }
     return collated_batch
 
 
 def sample_dataset(dataset, num_samples, seed, device):
+    """Z: randomly sample from dataset, return model input batch, visualization batch, image paths."""
     sampled_indices = sample_indices(len(dataset), num_samples, seed)
+    # Z: get samples
     samples = [dataset[index] for index in sampled_indices]
     inputs = torch.stack([sample["input"] for sample in samples], dim=0).to(device)
     imgs = torch.stack([sample["image"] for sample in samples], dim=0)
