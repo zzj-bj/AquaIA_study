ea324c4cc2e17034338af81108d52925cddb5d17
Zhijian ZHOU <zhijian2040.zhou@gmail.com>
2026-06-29 13:02:03 +0200

    logging/l__init__.py OK

--- detection/logging/__init__.py
+++ detection/logging/__init__.py
@@ -2,4 +2,5 @@ from detection.logging.training_logger import TrainingLogger
 from detection.logging.checkpoint_manager import CheckpointManager
 from detection.logging.run_registry import register_run, update_run_status
 
+# Z: when "from detection.logging import *" only import the following
 __all__ = ["TrainingLogger", "CheckpointManager", "register_run", "update_run_status"]
